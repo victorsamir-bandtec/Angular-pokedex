c773abc873d31fb2f85d6ecab4e3a629341f846c
victorsamir-bandtec <victor.bsilva@bandtec.com.br>
2021-10-02 10:04:57 -0300

    Add: adicionei todo layout da pagina

---
+++ pokedex/src/app/pages/pages.module.ts
@@ -0,0 +1,12 @@
+import { SharedModule } from './../shared/shared.module';
+import { RoutingModule } from './routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+
+@NgModule({
+  declarations: [HomeComponent, DetailsComponent],
+  imports: [CommonModule, RoutingModule, SharedModule],
+})
+export class PagesModule {}
