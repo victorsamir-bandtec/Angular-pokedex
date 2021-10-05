066f8e75bc9186828f7c1a3af0a02a5bb4b45efc
victorsamir-bandtec <victor.bsilva@bandtec.com.br>
2021-10-05 00:58:55 -0300

    add: not found find pokemon, production build

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -8,17 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeListComponent implements OnInit {
   private setAllPokemons: any;
-
-  public previousButton: boolean = false;
   public getAllPokemons: any;
+
+  public paginationButtons: boolean = true;
+  public previousButton: boolean = false;
+  public pokemonNotFound: boolean = false;
+  public apiError: boolean = false;
 
   constructor(private PokeApiService: PokeApiService) {}
 
   ngOnInit(): void {
-    this.PokeApiService.listAllPokemons.subscribe((res) => {
-      this.setAllPokemons = res.results;
-      this.getAllPokemons = this.setAllPokemons;
-    });
+    this.PokeApiService.listAllPokemons.subscribe(
+      (res) => {
+        this.setAllPokemons = res.results;
+        this.getAllPokemons = this.setAllPokemons;
+      },
+      (error) => {
+        this.apiError = true;
+        console.log(error);
+      }
+    );
   }
 
   public getSearch(value: string) {
@@ -26,7 +35,18 @@ export class PokeListComponent implements OnInit {
       return !res.name.indexOf(value.toLocaleLowerCase());
     });
 
+    this.paginationButtons = false;
     this.getAllPokemons = filter;
+
+    if (this.getAllPokemons.length === 10) {
+      this.paginationButtons = true;
+    }
+
+    if (this.getAllPokemons.length === 0) {
+      this.pokemonNotFound = true;
+    } else {
+      this.pokemonNotFound = false;
+    }
   }
 
   public nextPage() {
